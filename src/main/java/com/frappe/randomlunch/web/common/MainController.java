82a18e8c71cb894a2d592292276ecd0a79913e3c
SpellBound <junlee36520620@gmail.com>
2021-05-26 22:00:05 +0900

    menus save update

--- src/main/java/com/frappe/randomlunch/web/common/MainController.java
+++ src/main/java/com/frappe/randomlunch/web/common/MainController.java
@@ -1,16 +1,12 @@
 package com.frappe.randomlunch.web.common;
 
-import com.frappe.randomlunch.service.lunch.MenusService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.HttpServletRequest;
-import java.util.Map;
 
 @Controller
 @Slf4j
